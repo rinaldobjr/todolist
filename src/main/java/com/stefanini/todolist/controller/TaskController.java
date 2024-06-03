b98eec2e574767ffc64c096799bd8b60cd4f15c5
RinaldoBJr <rinaldobjr@gmail.com>
2024-06-03 15:28:56 -0400

    Implementado o EndPoints

--- src/main/java/com/stefanini/todolist/controller/TaskController.java
+++ src/main/java/com/stefanini/todolist/controller/TaskController.java
@@ -5,10 +5,14 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.stefanini.todolist.domain.entity.Task;
@@ -39,4 +43,25 @@ public class TaskController {
 		return new ResponseEntity<List<Task>>(this.taskService.listAllTasks(), HttpStatus.OK);
 	}
 	
+	@GetMapping(value="/{id}", produces = "application/json")
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<Task> getTaskById(@PathVariable (value= "id") Long id) {
+		log.info("Busca de uma tarefa por Id [{}]", id);
+		return this.taskService.findTaskById(id);
+	}
+	
+    @PutMapping(value = "/{id}", produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Task> updateTask(@RequestBody Task task, @PathVariable (value = "id") Long id)  {
+    	log.info("Atualização de uma tarefa por Id [{}] e as informações : [{}]", id, task);
+        return this.taskService.updateTaskById(task,id);
+    }
+	
+    @DeleteMapping(value="/{id}", produces = "application/json")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public ResponseEntity<Object> deleteTask(@PathVariable (value= "id") Long id) {
+    	log.info("Exclusão de uma tarefa por Id [{}]", id);
+		return this.taskService.deleteById(id);
+	}
+    
 }
